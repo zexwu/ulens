af42c7d414546fe4670c18c56803b2629add1540
Zexuan Wu <wuzexuan@pku.edu.cn>
2024-10-02 16:03:45 +0800

    add function pair

--- ulens_stats.py
+++ ulens_stats.py
@@ -30,3 +30,32 @@ def Dchi2(
     if verbose:
         print("Chi-square is reduced by", res.fun, "with extra d.o.f. =", len(args))
     return chi2_orig - res.fun
+
+
+def pair(t1: np.array, t2: np.array, tol: float = 0.01) -> tuple[np.array, np.array, np.array]:
+    """
+    INPUT :
+        t1, t2 [np.array] - two sorted array to be paired
+        tol[float] - tolerance for pairing
+
+
+    OUTPUT: the indices of the pairs w.r.t. t1 and t2.
+    """
+    ind1 = []
+    ind2 = []
+    diff = []
+
+    i1 = 0
+    i2 = 0
+
+    while i1 < len(t1) and i2 < len(t2):
+        if t2[i2] - t1[i1] > tol:
+            i1 += 1
+        elif t1[i1] - t2[i2] > tol:
+            i2 += 1
+        else:
+            ind1.append(i1)
+            ind2.append(i2)
+            diff.append(t1[i1] - t2[i2])
+            i1 += 1
+    return np.array(ind1), np.array(ind2), np.array(diff)
